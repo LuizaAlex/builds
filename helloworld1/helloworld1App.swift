898e17a602fd5dc9091da98f1bf4dd59f6f156c2
Luiza <poteca.luiza@gmail.com>
2023-04-08 15:15:08 -0700

    Initial Commit

---
+++ helloworld1/helloworld1App.swift
@@ -0,0 +1,17 @@
+//
+//  helloworld1App.swift
+//  helloworld1
+//
+//  Created by Luiza on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct helloworld1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
